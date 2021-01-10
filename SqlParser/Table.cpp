f073d3907ee6d4c0700d59f8f4edd4e4e4376739
Pascu <pascucosti9@gmail.com>
2021-01-10 14:21:11 +0200

    Updated table

--- SqlParser/Table.cpp
+++ SqlParser/Table.cpp
@@ -136,93 +136,55 @@ void Table::RemoveDataEntry(int columnPos, const string& value)
 
 void Table::UpdateDataEntry(int columnPos, const string& columnValue, const vector<string>& values)
 {
-	fstream file(_table_name + ".data", ios::in);
-
-	if (file.is_open()) {
-		string replace = "bar";
-		string replace_with = "foo";
-		string line;
-		vector<string> lines;
-
-		while (getline(file, line)) {
-			cout << line << endl;
-
-			string::size_type pos = 0;
-
-			while ((pos = line.find(replace, pos)) != string::npos) {
-				line.replace(pos, line.size(), replace_with);
-				pos += replace_with.size();
-			}
-
-vector<vector<string>> Table::ReadData()
-			lines.push_back(line);
-		}
-
-		file.close();
-		file.open(_table_name + ".data", ios::out | ios::trunc);
-
-		for (const auto& i : lines) {
-			file << i << endl;
-		}
-
-		file.close();
-	}
 }
 
 vector<string> Table::ReadData()
 {
 	string line;
-    ifstream myfile(_table_name + ".data", ios::binary);
+	ifstream myfile(_table_name + ".data", ios::binary);
 
-    if(!myfile.is_open())
-    {
-	   throw "Error opening the file!";		  
-   }
-
-   vector<vector<string>> lines;
-   int len = 0, valuei = 0;
-   float valuef = 0;
-   char* value = nullptr;
-
-   while (!myfile.eof())
-   {
-	   vector<string> line;
-	   for (int i = 0; i < _columns.size(); i++) {
-		   switch (_columns[i].GetType())
-		   {
-		   case DataTypes::Text:
-			   myfile.read((char*)&len, sizeof(int));
-			   value = new char[len + 1];
-			   myfile.read(value, len);
-			   value[len] = '\0';
-			   line.push_back(value);
-			   break;
-		   case DataTypes::Integer:
-			   myfile.read((char*)&valuei, sizeof(int));
-			   line.push_back(to_string(valuei));
-			   break;
-		   case DataTypes::Float:
-			   myfile.read((char*)&valuef, sizeof(float));
-			   line.push_back(to_string(valuef));
-			   break;
-		   default:
-			   break;
-		   }
-	   }
-	   lines.push_back(line);
-   }
-
-   delete value;
-
-   lines.pop_back();
-
-   return lines;
-    }
-
-    while(getline(myfile, line))
+	if (!myfile.is_open())
 	{
-		result.push_back(line);
-    }
-	myfile.close();
-	return result;
+		throw "Error opening the file!";
+	}
+
+	vector<vector<string>> lines;
+	int len = 0, valuei = 0;
+	float valuef = 0;
+	char* value = nullptr;
+
+	while (!myfile.eof())
+	{
+		vector<string> line;
+		for (int i = 0; i < _columns.size(); i++) {
+			switch (_columns[i].GetType())
+			{
+			case DataTypes::Text:
+				myfile.read((char*)&len, sizeof(int));
+				value = new char[len + 1];
+				myfile.read(value, len);
+				value[len] = '\0';
+				line.push_back(value);
+				break;
+			case DataTypes::Integer:
+				myfile.read((char*)&valuei, sizeof(int));
+				line.push_back(to_string(valuei));
+				break;
+			case DataTypes::Float:
+				myfile.read((char*)&valuef, sizeof(float));
+				line.push_back(to_string(valuef));
+				break;
+			default:
+				break;
+			}
+		}
+		lines.push_back(line);
+	}
+
+	delete value;
+
+	lines.pop_back();
+
+	return lines;
 }
+
